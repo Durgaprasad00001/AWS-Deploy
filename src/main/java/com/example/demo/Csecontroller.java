c16bad17565b700d2c1fe09f7e2f15da2aa2b452
Durgaprasad00001 <luckyprasad817@gmail.com>
2026-02-24 23:32:34 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Csecontroller.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Csecontroller {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Welcome to Jenkins AWS";
+    }
+
+    @GetMapping("/cse")
+    public String cse() {
+        return "Welcome to Third year cse";
+    }
+}
